5a41e4cd97bf3c442cafb33bf4bb24f297e1536a
Judongjae <wnehdwo0982@naver.com>
2022-05-28 20:57:04 +0900

    첫시작

---
+++ src/helloworld.java
@@ -0,0 +1,7 @@
+
+public class helloworld {//프로그래밍을 구현하려면 class내에 메인메서드를 만들어야한다.
+ public static void main(String[] orgs) {
+	 System.out.println("Hello World");
+	 System.out.println("fuckyou");
+ }
+}
