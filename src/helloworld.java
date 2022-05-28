0c49bb77a1d3d780238675273dd0ed836f8a5653
Judongjae <wnehdwo0982@naver.com>
2022-05-28 21:19:03 +0900

    드래그 앤 드롭으로 새롭게 commit

--- src/helloworld.java
+++ src/helloworld.java
@@ -3,5 +3,6 @@ public class helloworld {//프로그래밍을 구현하려면 class내에 메인
  public static void main(String[] orgs) {
 	 System.out.println("Hello World");
 	 System.out.println("fuckyou");
+	 System.out.println("i love you");
  }
 }
