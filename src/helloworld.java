6fd8d61420d6f2abe151bdf19801f8bcb6dcd0de
Judongjae <wnehdwo0982@naver.com>
2022-05-29 02:25:32 +0900

    java 할수있다!!

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,8 +1,5 @@
-
-public class helloworld {//프로그래밍을 구현하려면 class내에 메인메서드를 만들어야한다.
- public static void main(String[] orgs) {
-	 System.out.println("Hello World");
-	 System.out.println("fuckyou");
-	 System.out.println("i love you");
- }
+public class helloworld{
+	public static void main(String[] args) {
+		System.out.println("hey");
+	}
 }
