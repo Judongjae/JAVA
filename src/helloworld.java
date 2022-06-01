26efdf49454cfaa69632b2450a0b0ec6247dd945
Judongjae <wnehdwo0982@naver.com>
2022-06-02 00:34:22 +0900

    최악은 없어 이게 또 기회가 될지도 몰라 걱정말고 자자 안되면 공장가면되지 뭐 걱정만 하고 있지 말고 해야 할일을 하자

--- src/helloworld.java
+++ src/helloworld.java
@@ -1,8 +1,22 @@
-import java.util.Arrays;
-
 public class helloworld{
-	public static void main(String[] args){
-		long[] arr = {1,2,3,4,5};
-		System.out.println(Arrays.toString(arr[0]));
+	public static int[] calculate(int times) {//caculate라는 배열을 만듬, times로 입력받고 int[]로 출력한다.
+		int[] result = new int[9];
+		for(int i=0; i < result.length; i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	
+	public static void print(int[] result){//출력은 없고 입력만 있는 것이다.
+		for(int i=0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
+		}
 	}
 }
